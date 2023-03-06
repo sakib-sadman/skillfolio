1df1f651758e4c46c2f7b56dd4b43722fffbc024
GingerAle <ssakib183013@bscse.uiu.ac.bd>
2023-03-07 01:03:21 +0600

    login_part_done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoleManagementController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +19,38 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes(['verify' => true]);
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('admin_dashboard');
+});
+
+Route::get('/home', function () {
+    return redirect()->route('admin_dashboard');
+});
+
+Route::post('/user-login', [LoginController::class, 'LOGIN'])->name('LOGIN');
+
+#~~~~~~~~~~~~ all admin panel routes ~~~~~~~~~~~~~~
+
+Route::group(['prefix' => 'admin','middleware' => ['auth', 'role:admin']], function() {
+    
+    Route::get('/dashboard', [AdminController::class, 'dashboard'])->name('admin_dashboard');
+    
+
+
+    Route::get('/profile', [ProfileController::class, 'profile'])->name('profile');
+    Route::get('/profile/edit', [ProfileController::class, 'profile_edit'])->name('profile_edit');
+    Route::post('/profile/update', [ProfileController::class, 'profile_update'])->name('profile_update');
+
+    Route::get('/role-management', [RoleManagementController::class, 'index'])->name('role_management');
+    Route::post('/role-management/add', [RoleManagementController::class, 'role_add'])->name('role_add');
+    Route::get('/role-management/delete/{id}', [RoleManagementController::class, 'role_delete'])->name('role_delete');
+    Route::post('/role-management/update', [RoleManagementController::class, 'role_update'])->name('role_update');
+    Route::get('/get-role/{id}', [RoleManagementController::class, 'get_role'])->name('get_role');
+
+    Route::resource('users', UserController::class);
+    
+
+
+    Route::get('/user-account-status/{id}', [UserController::class, 'user_account_status_toggle'])->name('user-account-status-toggle');
 });
