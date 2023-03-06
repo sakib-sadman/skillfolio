6bc819029a82b1d1a5b3591d79fcb8997afc7b80
Jubaer_Shufol <jubaer.shufol@gmail.com>
2023-03-07 00:36:05 +0600

    added web

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\RoleManagementController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +19,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Auth::routes(['verify' => true]);
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('admin_dashboard');
+});
+
+Route::get('/home', function () {
+    return redirect()->route('admin_dashboard');
+});
+
+Route::post('/user-login', [LoginController::class, 'LOGIN'])->name('LOGIN');
+
+#~~~~~~~~~~~~ all admin panel routes ~~~~~~~~~~~~~~
+
+Route::group(['prefix' => 'admin','middleware' => ['auth', 'role:admin']], function() {
+    
+    
+    
+
+
+   
+    Route::resource('users', UserController::class);
+    
+
+
+    Route::get('/user-account-status/{id}', [UserController::class, 'user_account_status_toggle'])->name('user-account-status-toggle');
 });
