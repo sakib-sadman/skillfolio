f38bc2aeb46398714fd6b6cd4a2b1d8e428ada7c
Jubaer_Shufol <jubaer.shufol@gmail.com>
2023-03-07 01:09:52 +0600

    added user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,15 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-
+use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles ;
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +41,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function image() {
+        return $this->morphOne(Image::class, 'parentable');
+    }
+
+    public function images() {
+        return $this->morphMany(Image::class, 'parentable');
+    }
+
+   
 }
