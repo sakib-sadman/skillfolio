ef90ed4fdbac0d3d77a8df216070ff550549bf73
GingerAle <ssakib183013@bscse.uiu.ac.bd>
2023-04-27 20:39:19 +0600

    job_recommendations_done

--- app/Http/Controllers/FacultyController.php
+++ app/Http/Controllers/FacultyController.php
@@ -7,6 +7,54 @@ use Auth;
 use Illuminate\Support\Facades\Hash;
 class FacultyController extends Controller
 {
+
+    function recommendation_faculty_revoke_decision($id)
+    {
+        $job_recommendation = JobRecommendation::where('id',$id)->first();
+        $job_recommendation->status = 0;
+        $job_recommendation->save();
+
+        return back()->with('success', 'Recommendation Revoked to Pending');
+
+    }
+    function recommendation_faculty_approve($id)
+    {
+        $job_recommendation = JobRecommendation::where('id',$id)->first();
+        $job_recommendation->status = 1;
+        $job_recommendation->save();
+
+        return back()->with('success', 'Recommendation Accepted');
+
+    }
+
+    function recommendation_faculty_decline($id)
+    {
+        $job_recommendation = JobRecommendation::where('id',$id)->first();
+        $job_recommendation->status = -1;
+        $job_recommendation->save();
+
+        return back()->with('success', 'Recommendation Declined');
+
+    }
+
+    function recommendation_view($id)
+    {
+
+        $job_recommendation = JobRecommendation::where('id',$id)->where('faculty_id', Auth::id())->first();
+        $user = User::with('works','education', 'training','image')->find($job_recommendation->user_id);
+        return view('facultypanel.job_recommendation.view', compact('job_recommendation','user'));
+
+
+    }
+    function recommendation_list(){
+        $job_recommendations = JobRecommendation::where('faculty_id', Auth::id())->get();
+
+        $pending_job_recommendations = $job_recommendations->where('status', 0);
+        $accpetedDeclined_job_recommendations = $job_recommendations->where('status','!=', 0);
+        return view('facultypanel.job_recommendation.index', compact('pending_job_recommendations','accpetedDeclined_job_recommendations' ));
+    }
+
+
     function dashboard()
     {
         return view('facultypanel.dashboard.index');
--- routes/web.php
+++ routes/web.php
@@ -75,6 +75,11 @@ Route::group(['prefix' => 'faculty','middleware' => ['auth', 'role:faculty']], f
     Route::get('/profile/edit', [FacultyController::class, 'profile_edit'])->name('faculty_profile_edit');
     Route::post('/profile/update', [FacultyController::class, 'profile_update'])->name('faculty_profile_update');
 
+    Route::get('/student-job-recommendation/list', [FacultyController::class, 'recommendation_list'])->name('recommendation_list');
+    Route::get('/student-job-recommendation/{id}/view', [FacultyController::class, 'recommendation_view'])->name('recommendation_view');
+    Route::get('/student-job-recommendation/{id}/decline', [FacultyController::class, 'recommendation_faculty_decline'])->name('recommendation_faculty_decline');
+    Route::get('/student-job-recommendation/{id}/approve', [FacultyController::class, 'recommendation_faculty_approve'])->name('recommendation_faculty_approve');
+    Route::get('/student-job-recommendation/{id}/revoke', [FacultyController::class, 'recommendation_faculty_revoke_decision'])->name('recommendation_faculty_revoke_decision');
 });
 
 
@@ -87,5 +92,6 @@ Route::group(['prefix' => 'student','middleware' => ['auth', 'role:student']], f
     Route::get('/profile', [StudentController::class, 'profile'])->name('student_profile');
     Route::get('/profile/edit', [StudentController::class, 'profile_edit'])->name('student_profile_edit');
     Route::post('/profile/update', [StudentController::class, 'profile_update'])->name('student_profile_update');
+    Route::resource('job-recommendation', JobRecommendationController::class);  
 
 });
